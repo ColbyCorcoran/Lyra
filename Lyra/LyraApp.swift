d2658eaf5ab070a79611b887cdb2cf767abfc815
Colby Corcoran <colbyacorcoran@gmail.com>
2026-01-16 12:06:13 -0600

    Initial Commit

---
+++ Lyra/LyraApp.swift
@@ -0,0 +1,32 @@
+//
+//  LyraApp.swift
+//  Lyra
+//
+//  Created by Colby Corcoran on 1/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LyraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LyraTests/LyraTests.swift
@@ -0,0 +1,17 @@
+//
+//  LyraTests.swift
+//  LyraTests
+//
+//  Created by Colby Corcoran on 1/16/26.
+//
+
+import Testing
+@testable import Lyra
+
+struct LyraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
